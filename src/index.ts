78fbb08f09051a860516891272cefe3ad2e313e2
Thilo Reiner <thilo.reiner@googlemail.com>
2024-02-04 15:03:47 +0100

    feat: add basic story generation from JSDoc feat: add react-docgen-typescript dep

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
+import * as docgen from "react-docgen-typescript"
+import { convertArgsToCode, createStory, generateArgs, getTsxFiles } from './utils';
 
-console.log('hello world')
+const options = {
+  savePropValueAsString: true,
+};
+
+const path = process.argv.find(_ => _.startsWith("path="))?.replace('path=', '')
+
+if(path === undefined) {
+  console.error('No valid path argument provided. e.g. "storybook-autogen path=./src/components"')
+} else {
+  getTsxFiles(path).map(filePath => {
+    docgen.parse(filePath, options).forEach(component => {
+      let args = generateArgs(component)
+
+      const code = convertArgsToCode(component, args)
+
+      createStory(component, code)
+    })
+  })
+}
+
+
---
+++ src/utils.ts
@@ -0,0 +1,80 @@
+import * as fs from 'fs';
+import * as docgen from "react-docgen-typescript"
+
+export type PropValues = Record<string, Array<any>>
+export type GeneratedArgs = Record<string, any>
+
+export function getTsxFiles(folder: string) {
+  return fs.readdirSync(folder).filter(_ => _.includes('Button.tsx')).map(filename => {
+    return `${folder}/${filename}`
+  })
+}
+
+export function createStory(componentDoc: docgen.ComponentDoc, stories: string) {
+  fs.writeFileSync(componentDoc.filePath.replace('.tsx', '.stories.ts'), `
+  import type { Meta, StoryObj } from '@storybook/react';
+  import { ${componentDoc.displayName} } from './${componentDoc.displayName}';
+
+  const meta = {
+    component: ${componentDoc.displayName},
+  } satisfies Meta<typeof ${componentDoc.displayName}>;
+
+  export default meta;
+  type Story = StoryObj<typeof meta>;
+  ${stories}
+  `)
+}
+
+// TODO: This can be optimized
+export const getCombinations = (obj: PropValues): GeneratedArgs[] => {
+  let tmp = Object.entries(obj).reduce(
+    (acc, [key, values]) =>
+      acc.flatMap((combo) =>
+      values.map((value) => { return ({ ...combo, [key]: value })})
+      ),
+    [{}]
+  )
+
+  for (let i = 1; i < tmp.length; i++) {
+    for (const key in tmp[i]) {
+      const first = (tmp[0] as any)[key]
+      const curr =  (tmp[i] as any)[key]
+      if (first === curr) {
+        delete (tmp[i] as any)[key];
+      }
+    }
+  }
+  return tmp
+}
+
+// TODO: For now only required props are used
+export function generateArgs(componentDoc: docgen.ComponentDoc): GeneratedArgs[] {
+  let res: PropValues = {}
+  Object.keys(componentDoc.props).forEach(key => {
+    const val = componentDoc.props[key]
+    if(!val.required) return
+
+    try {
+      const autogenOccurence = val.description.match(/(@autogen).+/)?.[0].replace('@autogen', '').trim()
+      if(!autogenOccurence) return
+      const propValues = JSON.parse(autogenOccurence).values
+      res[key] = propValues
+    } catch(e) {
+      console.warn(e)
+      return
+    }
+  })
+  return getCombinations(res)
+}
+
+// TODO: use ComponentDoc to generate better story names
+export function convertArgsToCode(componentDoc: docgen.ComponentDoc, args: GeneratedArgs[]) {
+  return args.map((arg, index) => {
+    return `
+  export const ${index === 0 ? 'Default' : `Default${index}`}: Story = {
+    "args": {${index === 0 ? '' : '\n      ...Default.args,'}
+      ${JSON.stringify(arg, null, 6).slice(1, -1).trim()}
+    }
+  }`
+  }).join('\n')
+}
